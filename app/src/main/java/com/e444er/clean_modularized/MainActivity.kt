ee0e7a8dbed062404be068b21ed810d635de57e0
Bauka <e444er@gmail.com>
2022-11-19 17:46:18 +0600

    initial commit

---
+++ app/src/main/java/com/e444er/clean_modularized/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.e444er.clean_modularized
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
