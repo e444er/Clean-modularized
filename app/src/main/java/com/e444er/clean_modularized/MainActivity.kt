bfe75633b68e7e972570698b742700c42bd7138a
Bauka <e444er@gmail.com>
2022-11-20 22:12:03 +0600

    bottom navigation

--- app/src/main/java/com/e444er/clean_modularized/MainActivity.kt
+++ app/src/main/java/com/e444er/clean_modularized/MainActivity.kt
@@ -2,10 +2,51 @@ package com.e444er.clean_modularized
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.e444er.clean_modularized.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
+
+//    private lateinit var binding: ActivityMainBinding
+//
+////    private lateinit var navController: NavController
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+////        navController = findNavController(R.id.fragmentContainerView)
+//
+//
+////        setupActionBarWithNavController(navHostFragment.navController)
+//
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        binding.bottomNav.setupWithNavController(navController)
+//    }
+//
+////    override fun onSupportNavigateUp(): Boolean {
+////        return navController.navigateUp() || super.onSupportNavigateUp()
+////    }
+
 }
---
+++ home-feature/src/main/java/com/e444er/home_feature/presentation/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.e444er.home_feature.presentation.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.e444er.home_feature.R
+
+class HomeFragment: Fragment(R.layout.home_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
